348745c20e0c8dbc55eefcc4eae83bbd6600fa96
bjzhaoxin <bjzhaoxin@corp.netease.com>
2017-12-04 18:05:21 +0800

    add

--- majiang/src/Main.java
+++ majiang/src/Main.java
@@ -1,16 +1,37 @@
-import algorithm.HuTable;
-import algorithm.HuTableFeng;
-import algorithm.HuTableJian;
+import java.util.List;
+
+import algorithm.*;
 
 /**
  * Created by bjzhaoxin on 2017/11/17.
  */
 public class Main
 {
-	public static void main(String[] args)
+	public static void gen()
 	{
 		HuTableJian.gen();
 		HuTableFeng.gen();
 		HuTable.gen();
+	}
+
+	public static void load()
+	{
+		HuTableJian.load();
+		HuTableFeng.load();
+		HuTable.load();
+	}
+
+	public static void test()
+	{
+		String init = "1万,1万,1万,2筒,2条,3条,东,中";
+		List<Integer> cards = MaJiangDef.stringToCards(init);
+		System.out.println(HuUtil.isHu(cards, 3));
+	}
+
+	public static void main(String[] args)
+	{
+		//gen();
+		load();
+		test();
 	}
 }
---
+++ majiang/src/algorithm/HuUtil.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.List;
+
+/**
+ * Created by bjzhaoxin on 2017/12/4.
+ */
+public class HuUtil
+{
+	public static boolean isHu(List<Integer> cards, int guiIndex)
+	{
+		return false;
+	}
+}
