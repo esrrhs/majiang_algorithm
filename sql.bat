50ab853de920fd9b1c40f10adb7bfbb76f4f1375
bjzhaoxin <bjzhaoxin@corp.netease.com>
2017-12-06 15:10:42 +0800

    add

--- sql.bat
+++
@@ -1,9 +0,0 @@
-
-echo %date% %time%
-sqlite3.exe majiang.db < majiang_jian.sql
-echo %date% %time%
-sqlite3.exe majiang.db < majiang_feng.sql
-echo %date% %time%
-sqlite3.exe majiang.db < majiang.sql
-echo %date% %time%
-pause
